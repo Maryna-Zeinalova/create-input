5dc37ceca972fca9332612a9159e4c0a4446912c
Maryna Zeinalova <shmapetto@gmail.com>
2023-04-13 19:12:45 +0300

    added prototype methods

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function InputField (id, dom){    
-    this.id = id;
-    this.dom = dom;
-    const input = document.createElement('input');
-    dom.appendChild(input);
-    input.id = id;
-    this.isEmpty = function(){
-        if(input.value <= 0){
-            return true
-        } else {
-            return false
-        }
-    }
-    this.value = function(){
-        return input.value;
-    }
-    this.setValue = function(value){
-        input.value = value;
-    }
-    this.isPhone = function (){        
-        const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-        return regex.test(input.value);
-    }
+function InputField(id, dom) {
+  this.id = id;
+  this.dom = dom;
+  this.input = document.createElement('input');
+  dom.appendChild(this.input);
+  this.input.id = id;
 }
 
-const example = new InputField('example-id', document.querySelector('.container'));
+InputField.prototype.isEmpty = function () {
+  if (this.input.value <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+InputField.prototype.value = function () {
+  return this.input.value;
+};
+
+InputField.prototype.setValue = function (value) {
+  this.input.value = value;
+};
+
+InputField.prototype.isPhone = function () {
+  const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  return regex.test(this.input.value);
+};
+
+const example = new InputField(
+  'example-id',
+  document.querySelector('.container')
+);
